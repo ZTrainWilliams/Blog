36a50a989114b67f74de51d6362354890fb1a7f5
ZtrainWilliams <ztrain1224@163.com>
2022-11-11 11:50:31 +0800

    docs: 排序算法

---
+++ 算法/十大排序/3-InsertionSort.js
@@ -0,0 +1,29 @@
+// 插入排序（Insertion Sort）
+// 简单插入排序（Insertion-Sort）的算法描述是一种简单直观的排序算法。它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+// 属于比较类排序-插入排序；时间复杂度O(n^2),空间复杂度O(1),稳定性-稳定。
+
+// 算法描述：
+// 1、从第一个元素开始，该元素可以认为已经被排序；
+// 2、取出下一个元素，在已经排序的元素序列中从后向前扫描；
+// 3、如果该元素（已排序）大于新元素，将该元素移到下一位置；
+// 4、重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+// 5、将新元素插入到该位置后；
+// 6、重复步骤2~5。
+
+function insertionSort(arr) {
+  let len = arr.length;
+  let preIndex, current;
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1; // 已排序的最后一个值
+    current = arr[i];
+    while(preIndex >= 0 && arr[preIndex] > current) { // 大于当前值
+      arr[preIndex + 1] = arr[preIndex]; // 值替换
+      preIndex--;
+    }
+    arr[preIndex + 1] = current; // 比较完成，插入已排序
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 8, 6, 5, 4, 7, 9, 10, 1, 3]));
+
---
+++ 算法/十大排序/4-ShellSort.js
@@ -0,0 +1,27 @@
+// 希尔排序（Shell Sort）
+// 1959年Shell发明，第一个突破O(n2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+// 属于比较类排序-插入排序；时间复杂度O(n^1.3),空间复杂度O(1),稳定性-不稳定。
+
+// 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，算法描述：
+// 1、选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+// 2、按增量序列个数k，对序列进行k 趟排序；
+// 3、每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+
+function shellSort(arr) {
+  let len = arr.length;
+  for (let gap = Math.floor(len / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    // 分块，直至间隔变为1
+    for (let i = gap; i < len; i++) {
+      let j = i;
+      let current = arr[i];
+      while (j - gap >= 0 && arr[j - gap] > current) { // 插入判断替换
+        arr[j] = arr[j - gap];
+        j = j - gap;
+      }
+      arr[j] = current;
+    }
+  }
+  return arr;
+}
+
+console.log(shellSort([2, 8, 6, 5, 4, 7, 9, 10, 1, 3]));
